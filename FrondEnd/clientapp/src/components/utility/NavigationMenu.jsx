bd81c4cca27f5ae58ffd4ebebdcd9973fb6e90e8
muthuamr <muthu.amr@gmail.com>
2023-11-20 02:07:05 +0800

    Adding a brand link

--- FrondEnd/clientapp/src/components/utility/NavigationMenu.jsx
+++ FrondEnd/clientapp/src/components/utility/NavigationMenu.jsx
@@ -13,7 +13,7 @@ function NavigationMenu() {
     <>     
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="" style={{fontSize:"16px", color:"white"}}><p>Welcome to <strong>Muthu Store</strong></p></Navbar.Brand>
+          <Navbar.Brand href="/Products" style={{fontSize:"16px", color:"white"}}><p>Welcome to <strong>Muthu Store</strong></p></Navbar.Brand>
           <div class="ui menu">
           <Nav className="me-auto" style={{fontSize:"16px"}}>
             <Nav.Link style={{color:"black"}} href='/Customers'>Customers</Nav.Link>
